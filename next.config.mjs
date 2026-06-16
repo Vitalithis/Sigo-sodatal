e6f9f9d9679c61d5491126f10d627508a3e50333
Dan Ocampo <docampo@ing.ucsc.cl>
2026-06-16 18:15:36 -0400

    reincio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from 'next-pwa';
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  // Configuración de next.js
+};
+
+export default withPWA(nextConfig);
